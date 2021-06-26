dd161a94468eca74d6a1b36da85937c9738c1a64
Osazee Isere <oz1.concepts@gmail.com>
2021-06-26 05:53:26 +0100

    initial commit

---
+++ ComparableCar/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ComparableCar
+{
+  public  class Car: IComparable
+    {
+        //field variable
+        public const int MaxSpeed = 50;
+        private bool CarIsDead;//is the car dead
+        
+        // properties
+        public int CurrSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        public int CarID { get; set; }
+
+        //we now support a custom property to return
+        //the correct IComparer Interface
+        public static IComparer SortedByPetName
+        {
+            get { return (IComparer)new PetNameComparer(); }
+        }
+        //constructors
+        public Car()
+        {
+
+        }
+        public Car(string name,int speed,int id)
+        {
+            PetName = name;
+            CurrSpeed = speed;
+            CarID = id;
+        }
+
+        // this Car has a Radio(containment/delegation-inheritance)
+      //private  Radio carRadio = new Radio();
+
+       //methods
+       public void CrankTunes(bool state)
+        {
+            //delegate request to  inner object
+            //carRadio.RadioOn(state);
+        }
+
+         int IComparable.CompareTo(object obj)
+        {
+            //Car temp = obj as Car;
+            //if (temp != null)
+            //{
+                //if (this.CarID > temp.CarID)
+                //{
+                //    return 1;
+                //}
+                //if (this.CarID < temp.CarID)
+                //{
+                //    return -1;
+                //}
+                //since the c# int data type implement IComparable
+                //interface the above block of code be streamline as such
+                Car temp = obj as Car;
+                if (temp != null)
+            {
+                return this.CarID.CompareTo(temp.CarID);
+            }
+
+
+                else
+                {
+                    throw new ArgumentException("Parameter is not a car");
+
+                }
+            
+        }
+        // throw the custom carIsdeadException
+        //public void Accelerate(int delta)
+        //{
+
+        //            CarIsDeadException ex=
+        //             new CarIsDeadException($"{PetName} has overheated","You have a lead foot",
+        //             DateTime.Now);
+        //            ex.HelpLink = "http://www.CarRUs.com";
+
+
+        //            throw ex;
+
+
+        //}
+    }
+}
--- ComparableCar/Program.cs
+++ ComparableCar/Program.cs
@@ -10,6 +10,45 @@ namespace ComparableCar
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Fun with object sorting");
+            Car[] myCar = new Car[5];
+            myCar[0] = new Car("Bitto", 50, 1);
+            myCar[1] = new Car("Big for nothing", 80, 22);
+            myCar[2] = new Car("Pencil light", 90, 3);
+            myCar[3] = new Car("Crystal Light", 100, 6);
+            myCar[4] = new Car("Audi", 70, 44);
+            Console.WriteLine("Here is the unordered set of car");
+            foreach (Car c in myCar)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+                Console.WriteLine();
+            }
+                //now sort the car array
+                Array.Sort(myCar);
+                Console.WriteLine();
+
+            //now print sorted cars
+            foreach (Car c in myCar)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+                Console.WriteLine();
+            }
+
+            //now sort the car array by petName
+            //Array.Sort(myCar, new PetNameComparer());//made a bit cleaner below
+
+            Array.Sort(myCar, Car.SortedByPetName);
+
+            //now dump sorted cars
+            Console.WriteLine("Ordering By Pet Name");
+            foreach (Car c in myCar)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
